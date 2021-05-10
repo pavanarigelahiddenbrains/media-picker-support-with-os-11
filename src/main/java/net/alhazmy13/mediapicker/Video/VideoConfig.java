2b924ebd356073bc839c9391f4d39158613cfe47
Bhavesh Jethani <[pavankumar.arigela@hiddenbrains.in]>
2021-05-10 10:11:15 +0530

    init commit

---
+++ src/main/java/net/alhazmy13/mediapicker/Video/VideoConfig.java
@@ -0,0 +1,40 @@
+package net.alhazmy13.mediapicker.Video;
+
+import android.os.Environment;
+
+import java.io.Serializable;
+
+/**
+ * Created by Alhazmy13 on 8/16/16.
+ * MediaPicker
+ */
+class VideoConfig implements Serializable {
+
+    protected VideoPicker.Extension extension;
+    protected VideoPicker.Mode mode;
+    protected String directory;
+    protected boolean allowMultiple;
+    protected boolean isImgFromCamera;
+    protected boolean debug;
+
+    protected int requestCode = VideoPicker.VIDEO_PICKER_REQUEST_CODE;
+
+    VideoConfig() {
+        this.extension = VideoPicker.Extension.MP4;
+        this.mode = VideoPicker.Mode.CAMERA;
+        this.directory = Environment.getExternalStorageDirectory() + VideoTags.Tags.IMAGE_PICKER_DIR;
+        this.allowMultiple = false;
+    }
+
+    @Override
+    public String toString() {
+        return "ImageConfig{" +
+                "extension=" + extension +
+                ", mode=" + mode +
+                ", directory='" + directory + '\'' +
+                ", allowMultiple=" + allowMultiple +
+                ", isImgFromCamera=" + isImgFromCamera +
+                ", debug=" + debug +
+                '}';
+    }
+}
